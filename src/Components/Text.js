be589d6d16e8c820d275cfeca680baf7d40f0af5
Mahmoud Jarjour <mahmoudkarake1994m@gmail.com>
2021-10-06 15:37:08 +0300

    changing word color in a text

--- src/Components/Text.js
+++ src/Components/Text.js
@@ -1,38 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Text extends Component  {
-    render(){
-        return (
-            <div>
-                <p>
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type here is some text to type here is some text to type here is some text to type here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
-                here is some text to type 
+function Text(){
+    let word = "and";
 
-                </p>
-            </div>
-        ) ;
-    }
+    let text  = " Messages never and  our servers. a. The original words of something written or printed, as opposed to a paraphrase, translation, revision, or condensation. b. The words of a speech appearing in print. c. Words, as of a libretto, that are set to music in a composition. d. Words treated as data by a computer. e. A text message. 2. The body of a printed work as distinct from headings and illustrative matter on a page or from front and back matter in a book. "
+
+    const colors= ["red"];
+    const randomColor = () => {
+        return colors[(Math.random() * colors.length) >> 0];
+      };
+        
+      return (
+        <div>
+          {text.split(" ").map(word => {
+              if(word === "and")
+            return <span style={{ color: randomColor() }}>{`${word} `}</span>;
+                else
+                    return <span>{`${word} `}</span>
+          })}
+        </div>
+      );
+        
 }
+
 
 export default Text;
