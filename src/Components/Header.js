b1505251499b25bfbe8dfe847a68c8cf742aa89a
Mahmoud Jarjour <mahmoudkarake1994m@gmail.com>
2021-10-06 14:25:40 +0300

    add javaScript tag to Headers contain if Statement

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 function Header(){
+    
+    const client = "android";
+
+    const title_web = "Welcome To Web Area " ;
+    const title_others = "Welcome to development area  ";
+
    
     return (
         <div className="navbar bg-dark rounded">
-            <h1 className=" text-white">
-                Weclome to Web Development with React 
-            </h1>
+            <h1 className=" text-white"> {  client === "web" ? title_web : title_others } </h1>
         </div>
 
     );
